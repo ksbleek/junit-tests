20a826801912cfc14b934cb93bdfd6fa9d33c113
ksbleek <kyle.s.bleeker@gmail.com>
2023-11-21 10:14:13 -0600

    finished part 1 of exercise

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
+
 public class Student {
+    private long id;
+    private String name;
+    private ArrayList<Integer> grades;
+
+    public Student(long id, String name) {
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Integer> getGrades() {
+        return grades;
+    }
+
+    public void addGrade(int grade) {
+        this.grades.add(grade);
+    }
+    public double getGradeAverage() {
+        int sum = 0;
+        for (int grade : this.grades) {
+            sum += grade;
+        }
+        return this.grades.isEmpty() ? 0 : (double) sum / this.grades.size();
+    }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,2 +1,57 @@
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class StudentTest {
+    private Student student;
+
+    @BeforeEach
+    public void setUp() {
+        student = new Student(1L, "John Doe");
+        student.addGrade(90);
+        student.addGrade(85);
+        student.addGrade(88);
+    }
+
+    @Test
+    public void testGetId() {
+        assertEquals(1L, student.getId());
+    }
+
+    @Test
+    public void testGetName() {
+        assertEquals("John Doe", student.getName());
+    }
+
+    @Test
+    public void testGetGrades() {
+        assertEquals(new ArrayList<>(Arrays.asList(90, 85, 88)), student.getGrades());
+    }
+
+    @Test
+    public void testAddGrade() {
+        student.addGrade(95);
+        assertTrue(student.getGrades().contains(95));
+    }
+
+    @Test
+    public void testGetGradeAverage() {
+        Student student = new Student(1, "John");
+        student.addGrade(85);
+        student.addGrade(90);
+        student.addGrade(95);
+        assertEquals(90.0, student.getGradeAverage());
+    }
+
+    @Test
+    public void testGetGradeAverageEmptyList() {
+        Student student = new Student(2, "Jane");
+        assertEquals(0.0, student.getGradeAverage());
+    }
 }
